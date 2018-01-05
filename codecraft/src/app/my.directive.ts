e61118e37016380e1ddbae966364c6acdc05fef4
Alex Kibler <alexkibler@me.com>
2018-01-05 12:39:27 -0500

    complete host listener and host binding lesson

--- codecraft/src/app/my.directive.ts
+++ codecraft/src/app/my.directive.ts
@@ -1,11 +1,25 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[ccCardHover]'
 })
 export class CardHoverDirective {
+  private isHovering: boolean = false
+
   constructor (private el: ElementRef,
                private renderer: Renderer) {
     renderer.setElementStyle(el.nativeElement, 'background-color', 'gray')
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    let punchlineEl = this.el.nativeElement.querySelector('.card-text')
+    this.renderer.setElementStyle(punchlineEl, 'display', 'block')
+    this.isHovering = true
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    let punchlineEl = this.el.nativeElement.querySelector('.card-text')
+    this.renderer.setElementStyle(punchlineEl, 'display', 'none')
+    this.isHovering = false
   }
 }
