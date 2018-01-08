396765b66335b6cbcb392d8098dcf87162a164df
Alex Kibler <alexkibler@me.com>
2018-01-08 14:06:25 -0500

    more pipe practice

--- codecraft/src/app/my.pipe.ts
+++ codecraft/src/app/my.pipe.ts
@@ -4,10 +4,14 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'my'
 })
 export class MyPipe implements PipeTransform {
-  transform(value: string, fallback: string): any {
+  transform(value: string, fallback: string, forceHttps: boolean = false): any {
     let image
     image = value ? value : fallback
+    if (forceHttps) {
+      if (image.indexOf('https') === -1) {
+        image = image.replace('http', 'https')
+      }
+    }
     return image
   }
-
 }
