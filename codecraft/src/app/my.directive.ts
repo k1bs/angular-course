4edf8609c6020c16cc8171dfefbe6565aefffa27
Alex Kibler <alexkibler@me.com>
2018-01-08 12:08:56 -0500

    inputs and config

--- codecraft/src/app/my.directive.ts
+++ codecraft/src/app/my.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[ccCardHover]'
@@ -6,19 +6,23 @@ import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@ang
 export class CardHoverDirective {
   @HostBinding('class.card-outline-primary·') private isHovering: boolean = false
 
+  @Input() config: Object = {
+    querySelector: '.card-text'
+  }
+
   constructor (private el: ElementRef,
                private renderer: Renderer) {
-    renderer.setElementStyle(el.nativeElement, 'background-color', 'gray')
+    // renderer.setElementStyle(el.nativeElement, 'background-color', 'gray')
   }
 
   @HostListener('mouseover') onMouseOver() {
-    let punchlineEl = this.el.nativeElement.querySelector('.card-text')
+    let punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector)
     this.renderer.setElementStyle(punchlineEl, 'display', 'block')
     this.isHovering = true
   }
 
   @HostListener('mouseout') onMouseOut() {
-    let punchlineEl = this.el.nativeElement.querySelector('.card-text')
+    let punchlineEl = this.el.nativeElement.querySelector(this.config.querySelector)
     this.renderer.setElementStyle(punchlineEl, 'display', 'none')
     this.isHovering = false
   }
